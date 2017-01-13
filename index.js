10904240452159e246ed7a7800acf0e97145e8e8
VieZhong <zyw13579@qq.com>
2017-01-13 11:42:37 +0800

    add database

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-var fs  = require('co-fs');
+var fs = require('co-fs');
 var logger = require('koa-logger');
 var route = require('koa-route');
+var staticServe = require('koa-static');
 var koa = require('koa');
 
 var app = koa();
 
 app.use(logger());
 
-app.use(route.get('/bookstore/index.html', function* (next) {
-    this.body = yield fs.readFile('./apps/bookstore/index.html', 'utf8');
+app.use(staticServe('./apps/'));
+
+app.use(route.get('/api/bookstore/books/hasRead', function* (next) {
+    this.response.body = JSON.parse(yield fs.readFile('./database/bookstore/hasRead.js', 'utf8'));
 }));
 
-app.use(route.get('/bookstore/bundle.js', function* (next) {
-    this.body = yield fs.readFile('./apps/bookstore/bundle.js', 'utf8');
+app.use(route.get('/api/bookstore/books/willRead', function* (next) {
+    this.response.body = JSON.parse(yield fs.readFile('./database/bookstore/willRead.js', 'utf8'));
 }));
 
-app.listen(80);
+app.listen(80, () => {
+    console.log('listening on port 80');
+});
