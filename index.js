a9847eae16bd500b6daff89fa532586169b327a2
VieZhong <zyw13579@qq.com>
2017-01-18 15:05:11 +0800

    bring in koa-seesion && add interface logout

--- index.js
+++ index.js
@@ -3,20 +3,45 @@ var parse = require('co-body');
 var logger = require('koa-logger');
 var route = require('koa-route');
 var staticServe = require('koa-static');
+var session = require('koa-session');
 var koa = require('koa');
 
 var app = koa();
 
+app.keys = ['koajs'];
+
 app.use(logger());
+app.use(session(app));
 
 app.use(staticServe('./apps/'));
 
 app.use(route.post('/api/login', function* () {
     let user = yield parse.form(this);
     if(user.account == 'xiaofu' && user.password == '584662') {
+        this.session.authed = true;
         this.response.status = 200;
+        this.response.body = {'account': user.account, 'name': 'VieZhong'};
     }else {
         this.response.status = 400;
+    }
+}));
+
+app.use(route.post('/api/logout', function* () {
+    this.session.authed = false;
+    this.response.status = 200;
+}));
+
+app.use(route.get('/api/users/me', function* () {
+    if(!this.session.authed){
+        this.response.body = {
+            authed: false
+        }
+    }else{
+        this.response.body = {
+            authed: true,
+            account: 'xiaofu',
+            name: 'VieZhong'
+        }
     }
 }));
 
@@ -46,6 +71,9 @@ app.use(route.get('/api/bookstore/books/:db/:id', function* (db, id) {
 }));
 
 app.use(route.post('/api/bookstore/books/:db', function* (db) {
+    if(!this.session.authed){
+        return;
+    }
     if(!['hasRead', 'willRead'].includes(db)){
         this.response.body = '请求方法不存在！';
         this.response.status = 404;
@@ -74,6 +102,9 @@ app.use(route.post('/api/bookstore/books/:db', function* (db) {
 }));
 
 app.use(route.put('/api/bookstore/books/:db/:id', function* (db, id) {
+    if(!this.session.authed){
+        return;
+    }
     if(!['hasRead', 'willRead'].includes(db)){
         this.response.body = '请求方法不存在！';
         this.response.status = 404;
@@ -117,6 +148,9 @@ app.use(route.put('/api/bookstore/books/:db/:id', function* (db, id) {
 }));
 
 app.use(route.delete('/api/bookstore/books/:db/:id', function* (db, id) {
+    if(!this.session.authed){
+        return;
+    }
     if(!['hasRead', 'willRead'].includes(db)){
         this.response.body = '请求方法不存在！';
         this.response.status = 404;
@@ -133,6 +167,36 @@ app.use(route.delete('/api/bookstore/books/:db/:id', function* (db, id) {
         book.id = i + 1;
     });
     fs.writeFile(`./database/bookstore/${db}.js`, JSON.stringify(booksList), {'encoding': 'utf8'});
+}));
+
+app.use(route.post('/api/bookstore/books/hasReadFromWillRead/:id', function* (id) {
+    if(!this.session.authed){
+        return;
+    }
+    let willReadList = JSON.parse(yield fs.readFile(`./database/bookstore/willRead.js`, 'utf8'));
+    let result = willReadList.find((book) => id == book.id);
+    if(!result){
+        this.response.body = '该书不存在！';
+        this.response.status = 404;
+        return;
+    }
+    let hasReadList = JSON.parse(yield fs.readFile(`./database/bookstore/hasRead.js`, 'utf8'));
+    let isExisted = hasReadList.some((book) => ['name', 'country', 'author'].every((key) => result[key] == book[key]));
+    if(isExisted){
+        this.response.status = 409;
+        this.response.body = '该书已存在于阅读记录！';
+        return;
+    }
+    willReadList.sort((x, y) => x.id - y.id).splice(id - 1, 1);
+    willReadList.forEach((book, i) => {
+        book.id = i + 1;
+    });
+    hasReadList.push(Object.assign(result, {
+        id: hasReadList.length + 1
+    }));
+    this.response.status = 200;
+    fs.writeFile(`./database/bookstore/hasRead.js`, JSON.stringify(hasReadList), {'encoding': 'utf8'});
+    fs.writeFile(`./database/bookstore/willRead.js`, JSON.stringify(willReadList), {'encoding': 'utf8'});
 }));
 
 app.listen(80, () => {
