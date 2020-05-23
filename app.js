a302944549f5acd50c786ccbdae6c97fee3d5511
VieZhong <zyw13579@qq.com>
2020-05-23 20:19:15 +0800

    feat: 数字生成

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app
             ctx.status = 301;
             ctx.redirect('/drawlots/index.html');
             return;
+        } else if (ctx.path === '/num') {
+            ctx.status = 301;
+            ctx.redirect('/num/index.html');
+            return;
         } else if (['/communication', '/communication/login', '/communication/chat', '/communication/box','/communication/contact'].includes(ctx.path)) {
             ctx.set('Content-Type', 'text/html');
             ctx.body = fs.readFileSync('apps/communication/index.html');
